d2ea3f8660fcf4bb1694b580fff7d75a6428d60b
pisue <pisue22@naver.com>
2023-03-29 14:37:26 +0900

    Day_4 완료

---
+++ src/main/kotlin/Day_4.kt
@@ -0,0 +1,20 @@
+import java.lang.Math.ceil
+
+class Day_4 {
+    //피자 나눠 먹기(1)
+    fun no120814(n: Int): Int = if (n % 7 == 0) n / 7 else (n / 7) + 1
+    fun no120814Other(n: Int) = ceil(n / 7.0) //ceil은 반올림
+
+    //피자 나눠 먹기(3)
+    fun no120816(slice: Int, n: Int) = ceil((n / slice.toDouble()))
+
+    //배열의 평균값
+    fun no120817(numbers: IntArray) = numbers.average()
+
+
+
+
+
+
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
+
 fun main(args: Array<String>) {
     println("Hello World!")
-
-    var IntArray = [1,2,3,4,5]
-    println(IntArray)
+    val IntArray = intArrayOf(1, 2, 3, 4, 5)
+    println("IntArray:$IntArray")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
